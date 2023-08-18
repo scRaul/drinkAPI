054727e848c35db84fc32d1872b63ff60aca1106
scRaul <61263078+scRaul@users.noreply.github.com>
2023-08-18 15:12:45 -0700

    updated put request

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,4 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const drink = require("../models/drink");
 const Drink = require("../models/drink");
+const { fileURLToPath } = require("url");
 exports.addADrink = (req,res,next ) =>{
     const name = req.body.name;
     const ingrediantList = req.body.ingrediantList; 
@@ -57,12 +62,49 @@ exports.getDrinkByName = (req,res,next) => {
 }
 
 exports.updateDrink = (req,res,next) =>{
-    res.status(200).json({
-        updateDrink: {name: "thisDrink",description:"updatedDrink"}
-    });
+    const drinkName = req.params.drinkName;
+    const description = req.body.description;
+    const ingrediantList = req.body.ingrediantList;
+    let imageUrl = req.body.image;
+    var fileUploaded = false;
+    if ( req.file ){
+        imageUrl = req.file.path;
+        fileUploaded = true;
+    }
+    if( !imageUrl ){
+        throw new Error("no file picked");
+        next();
+    }
+
+    Drink.findOne({name:drinkName })
+        .then(drink =>{
+            if( !drink ){
+                throw new Error('nil');
+            }
+            if(fileUploaded){
+                clearImage(drink.imageUrl);
+            }
+            drink.name = drinkName;
+            drink.description = description;
+            drink.ingrediantList = ingrediantList;
+            drink.imageUrl = imageUrl;
+            return drink.save();
+        })
+        .then(result => {
+            res.status(200).json({message:"drink updated!", drink: result });
+        })
+        .catch(err =>{
+            next(err);
+        })
+
 }
 exports.deleteDrink = (req,res,next) =>{
     res.status(200).json({
         deleteDrink: {name: "deletedDrink",description:"thisDrink"}
     });
+}
+
+const clearImage = filePath =>{
+    filePath = path.join(__dirname,'..',filePath);
+    fs.unlink(filepath,err => console.log(err));
 }
