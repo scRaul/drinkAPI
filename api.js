4f9fff9029fc2cdbf6b36a6057120cc67d49c26c
scRaul <61263078+scRaul@users.noreply.github.com>
2023-09-05 21:34:47 -0700

    fixed misspelled bug

--- api.js
+++ api.js
@@ -36,7 +36,7 @@ api.use(fileParser);
 api.use((req,res,next) => {
    res.setHeader('Access-Control-Allow-Origin','*');
    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE ');
-   res.setHeader('Aceess-Control-Allow-Header','Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Header','Content-Type, Authorization');
 
    next();
 });
