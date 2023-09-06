17fb9af322e545a0af2fea901867ae3b701bc862
scRaul <61263078+scRaul@users.noreply.github.com>
2023-09-06 13:50:58 -0700

    ..

--- api.js
+++ api.js
@@ -35,10 +35,13 @@ api.use(fileParser);
 //CORS
 api.use((req,res,next) => {
    res.setHeader('Access-Control-Allow-Origin','*');
-   res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE ');
+   res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS');
    res.setHeader('Access-Control-Allow-Header','Content-Type, Authorization');
 
    next();
+});
+api.options((req,res) =>{
+   res.status(204).end();
 });
 
 //api.use(multer().none());
